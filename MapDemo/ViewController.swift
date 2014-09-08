715e528d06a8b0c801a004060299ab61ef3d17c1
tsuchikazu <huneyhunt77@gmail.com>
2014-09-09 00:23:54 +0900

    Initial Commit

---
+++ MapDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapDemo
+//
+//  Created by 土屋 和良 on 2014/09/09.
+//  Copyright (c) 2014年 Kazuyoshi Tsuchiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
